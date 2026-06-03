7e22d6d289faa4db5b95a96cff6ec8a5585f87a0
Chace Suchintamai <csuchint0001@mymail.lausd.net>
2026-06-03 11:51:17 -0700

    day 3

--- src/main/java/com/chaceybacey/FinalFinch.java
+++ src/main/java/com/chaceybacey/FinalFinch.java
@@ -7,8 +7,35 @@ public class FinalFinch extends Finch {
     public FinalFinch(String device) {
         super(device);
     }
-
     
+
+     public void setColors(){
+        int patternArray[] = {0,1,1,1,0,1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,0,1,1,1,0};
+        setDisplay(patternArray);
+        setBeak(0,100,0);
+        for(int i = 1; i <= 4; i++){
+            setTail(i,0,100,0);
+        }
+      }
+
+    public void moveForward(double distance, double speed){
+        setMotors(50, 50);
+        setMove("F", distance, speed);
+        pause(0.5);
+    }
+
+    public void turnDirection(String direction){
+        int finchRight = 25;
+        int finchNorth = 350;
+        if(getCompass()< finchNorth || getCompass() > finchRight) {
+            setTurn(direction, 90, 50);
+        }
+    }
+    
+    // public void stopRobot() {
+
+    // }
+      
 
 
 
--- src/main/java/com/chaceybacey/TesterFinch.java
+++ src/main/java/com/chaceybacey/TesterFinch.java
@@ -4,15 +4,33 @@ public class TesterFinch {
    public static void main(String[] args) {
        FinalFinch bird = new FinalFinch("A");
 
-       bird.setMotors(50, 50);
 
+       bird.setColors();
 
-      public static void setColors(){
-        int patternArray[] = {0,0,0,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,1,0,0,0,0,0};
-        bird.setDisplay(patternArray);
+      for(int i = 0; i < 4; i++){
+        if(i % 2 == 0){
+         bird.moveForward(57,50);
+         bird.turnDirection("L");
+         bird.moveForward(23,50);
+         bird.turnDirection("L");  
+        }
+        if(i % 2 == 1){   
+         bird.moveForward(57,50);
+         bird.turnDirection("R");
+         bird.moveForward(25,50);
+         bird.turnDirection("R");
+        }
       }
       
        
+       bird.turnDirection("R");
+       bird.moveForward(55,70);
+
+       
+       System.out.println(bird.getCompass());
+       bird.stopAll();
+
+       // 
    
    }
 
