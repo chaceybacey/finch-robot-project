1105099e6959ab7af4c2a1ac7984aa4bbb6c305f
Chace Suchintamai <csuchint0001@mymail.lausd.net>
2026-06-05 12:32:41 -0700

    comments
    added the comments to each of the methods and important bodies of code

--- src/main/java/com/chaceybacey/FinalFinch.java
+++ src/main/java/com/chaceybacey/FinalFinch.java
@@ -8,6 +8,9 @@ public class FinalFinch extends Finch {
         super(device);
     }
     
+     /* The method setColors() sets the LED lights on the body and tail of the Finch robot to green. 
+      * It also sets the display on the robot to the number 8. 
+      */
 
      public void setColors(){
         int patternArray[] = {0,1,1,1,0,1,0,0,0,1,0,1,1,1,0,1,0,0,0,1,0,1,1,1,0};
@@ -18,15 +21,25 @@ public class FinalFinch extends Finch {
         }
       }
 
+    /* The method moveForward() makes the Finch robot move forward based on parameters distance & speed
+     * @param distance - the amount of centimeters Finch will travel
+     * @param speed - the speed of the Finch motors and how fast you want it to travel
+     */
     public void moveForward(double distance, double speed){
         setMotors(50, 50);
         setMove("F", distance, speed);
         pause(0.5);
     }
-
+    
+    /*
+     * The method turnDirection() utilizes the getCompass() method from the Finch library. 
+     * if the compass value is less than or over a certain threshold, it will turn in the direction in parameters. 
+     * @param direction - the directions "R" or "L" to turn right or left
+     */ 
     public void turnDirection(String direction){
         int finchRight = 25;
         int finchNorth = 350;
+        // this conditional statement will make it turn if the value return by getcompass is less than 350 or more than 25.
         if(getCompass()< finchNorth || getCompass() > finchRight) {
             setTurn(direction, 90, 50);
         }
--- src/main/java/com/chaceybacey/TesterFinch.java
+++ src/main/java/com/chaceybacey/TesterFinch.java
@@ -3,10 +3,12 @@ package com.chaceybacey;
 public class TesterFinch {
    public static void main(String[] args) {
        FinalFinch bird = new FinalFinch("A");
+          
+      // sets the colors of the robot to green.
+         bird.setColors();
 
-
-       bird.setColors();
-
+      // this loop makes Finch moveforward and turn right or left based on the index of i. 
+      // for example if i is 0, it will turn left. If i is 1, it will turn right.
       for(int i = 0; i < 4; i++){
         if(i % 2 == 0){
          bird.moveForward(57,50);
@@ -22,12 +24,11 @@ public class TesterFinch {
         }
       }
       
-       
+       // turns the robot left after reaching checkpoint 1 and moves it forward.
        bird.turnDirection("R");
        bird.moveForward(55,70);
 
        
-       System.out.println(bird.getCompass());
        bird.stopAll();
 
        
